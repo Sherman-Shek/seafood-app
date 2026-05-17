037fe594e6070c44a02b62aaf46abecb38c2c33e
Sherman-Shek <shermaning4@gmail.com>
2026-05-17 16:47:12 +0800

    Fix: add helmet in App.js

--- seafood-frontend/src/App.js
+++ seafood-frontend/src/App.js
@@ -37,6 +37,13 @@ function App() {
   return (
     <HelmetProvider>
       <div>
+
+        <Helmet>
+        {/* 這行是關鍵！如果子頁面沒有標題，就顯示這個 */}
+        <title>永生發海鮮 | Wing Sang Fat</title>
+        <meta name="description" content="新鮮海鮮批發與零售" />
+      </Helmet>
+
         <Navbar />
         <Routes>
           <Route path="/" element={<Navigate to="/en" replace />} />
